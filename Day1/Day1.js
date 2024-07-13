dbec50b4c0686a628399ee89c40e314a93938659
ShivamBhuskute <bhuskuteshivam8@gmail.com>
2024-07-13 13:13:42 +0530

    day1

---
+++ Day1/Day1.js
@@ -0,0 +1,30 @@
+// Activity 1
+var num = 5;
+console.log(num);
+
+let str = "srtin";
+console.log(str);
+
+// Activity 2
+const bool = true;
+console.log(bool);
+
+// Activity 3
+let x1 = 5;
+console.log(typeof x1);
+
+let x2 = "string";
+console.log(typeof x2);
+
+let x3 = {}
+console.log(typeof(x3));
+
+// Activity 4
+let x4 = 5;
+x4 = 4
+console.log(x4);
+
+// Activity 5
+const x5 = 5
+x5 = 6;
+console.log(x5);
---
+++ Day1/featureRequest.js
@@ -0,0 +1,11 @@
+let x1 = 5
+let x2 = "string"
+let x3 = true
+
+console.log(x1, typeof(x1));
+
+
+// Reassignment of operators
+const x4 = 4;
+x4 = 5;
+x1 = 2;
