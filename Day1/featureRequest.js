13e45a1e16cf222aebf00a64d3a8495643eb7021
ShivamBhuskute <bhuskuteshivam8@gmail.com>
2024-07-13 13:27:54 +0530

    new file

--- Day1/featureRequest.js
+++ Day1/featureRequest.js
@@ -1,11 +1,14 @@
-let x1 = 5
-let x2 = "string"
-let x3 = true
+let x1 = 5;
+let x2 = "string";
+let x3 = true;
 
-console.log(x1, typeof(x1));
-
+console.log(x1, typeof x1);
+console.log(x2, typeof x2);
+console.log(x3, typeof x3);
 
 // Reassignment of operators
 const x4 = 4;
-x4 = 5;
+// x4 = 5;
 x1 = 2;
+
+
