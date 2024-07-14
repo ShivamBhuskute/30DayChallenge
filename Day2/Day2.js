87cadfc1c0aedab81191e4e87837fab7f56d267b
ShivamBhuskute <bhuskuteshivam8@gmail.com>
2024-07-14 21:19:29 +0530

    day2

---
+++ Day2/Day2.js
@@ -0,0 +1,39 @@
+// Activity 1
+console.log(5 + 2);
+console.log(5 - 2);
+console.log(5 / 2);
+console.log(5 * 2);
+console.log(5 % 2);
+
+// Actuvity 2
+var a = 5;
+a += 5;
+console.log(a);
+
+a -= 5;
+console.log(a);
+
+// Activity 3
+5 + 2 > 3 ? console.log(true) : console.log(false);
+5 + 2 < 3 ? console.log(true) : console.log(false);
+5 + 2 <= 3 ? console.log(true) : console.log(false);
+a = 5 - 3 >= 3 ? console.log(true) : console.log(false);
+
+5 == 3 ? console.log(true) : console.log(false);
+
+5 === 3 ? console.log(true) : console.log(false);
+
+// Activity 4
+if (5 == 5 && 3 == 3) {
+  console.log("ok");
+}
+if (5 == 5 || 3 == 3) {
+  console.log("ok");
+}
+if (!a) {
+  console.log("ok");
+}
+
+// Activity 5
+num = 5;
+num > 0 ? console.log("positive") : console.log("negative");
