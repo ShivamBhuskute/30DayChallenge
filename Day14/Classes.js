3f288abdc348109bfaa04de26c53404b80d61854
ShivamBhuskute <bhuskuteshivam8@gmail.com>
2024-07-26 13:10:51 +0530

    Activity 2 of day 14 (classes)

--- Day14/Classes.js
+++ Day14/Classes.js
@@ -8,10 +8,26 @@ class Person {
     }
     updatedAge(age) {
         this.age = age;
-        return `Ner age is ${age}`
+        return `Her age is ${age}`;
     }
-} 
+}
 
 const person1 = new Person();
-console.log(person1.greet()); 
-console.log(person1.updatedAge(20)); 
+console.log(person1.greet());
+console.log(person1.updatedAge(20));
+
+// Activity 2
+class Student extends Person {
+    studentId = 111;
+    whatId() {
+        return `ID of the student is ${this.studentId}`;
+    }
+    greet() {
+        super.greet();
+        return `hi! ${this.studentId}, ${this.name}, ${this.age}!!`;
+    }
+}
+
+const student1 = new Student();
+console.log(student1.studentId);
+console.log(student1.greet());
