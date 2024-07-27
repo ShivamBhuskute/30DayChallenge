62bdf1d122ecea983811b90b35c59825c93323fc
ShivamBhuskute <bhuskuteshivam8@gmail.com>
2024-07-27 19:14:10 +0530

    Activity 1 and 2 of Day 15(Closures)

---
+++ Day15/closure.js
@@ -0,0 +1,35 @@
+// Activity 1
+function outerFunction() {
+    let outerVariable = "Hello, world!";
+
+    function innerFunction() {
+        return outerVariable;
+    }
+
+    return innerFunction;
+}
+
+const myInnerFunction = outerFunction();
+console.log(myInnerFunction()); // Logs 'Hello, world!'
+
+function createCounter() {
+    let counter = 0; // Private variable
+
+    return {
+        increment: function () {
+            counter += 1;
+        },
+        getValue: function () {
+            return counter;
+        },
+    };
+}
+
+const myCounter = createCounter();
+
+myCounter.increment();
+myCounter.increment();
+console.log(myCounter.getValue()); // Logs 2
+
+myCounter.increment();
+console.log(myCounter.getValue()); // Logs 3
